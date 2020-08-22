f95d71d5a2677e68eb2d8fd43415e881925b38aa
Scott <sagreine@gmail.com>
2020-08-22 11:54:56 -0400

    program pick bones, Go bones in

--- lib/views/pick_day.dart
+++ lib/views/pick_day.dart
@@ -7,8 +7,10 @@ class PickDay extends StatefulWidget {
 }
 
 class _PickDayState extends State<PickDay> {
+  // this is an obviously bad idea
   List<String> exercises = ["Squat", "Deadlift", "Bench", "Press"];
   List<bool> selectedExercise = [false, false, false, false];
+  bool readyToGo = false;
 
   @override
   Widget build(BuildContext context) {
@@ -34,78 +36,121 @@ class _PickDayState extends State<PickDay> {
         ),
         body: ListView(
           children: [
-            SizedBox(
-              height: 20,
-              child: Text("Select an exercise for today"),
-            ),
-            SizedBox(
-              height: 400,
-              child: GridView.count(
-                mainAxisSpacing: 2,
-                crossAxisSpacing: 2,
-                crossAxisCount: 2,
-                children: List.generate(4, (index) {
-                  return AnimatedContainer(
-                    decoration: BoxDecoration(
-                      gradient: selectedExercise[index]
-                          ? LinearGradient(
-                              colors: <Color>[
-                                Color(0xFF0D47A1),
-                                Color(0xFF1976D2),
-                                Color(0xFF42A5F5),
-                              ],
-                            )
-                          : LinearGradient(
-                              colors: <Color>[
-                                Color(0xFF058f43),
-                                Color(0xFF06ac51),
-                                Color(0xFF1bcc50),
-                              ],
-                            ),
-                    ),
-                    //color: isPickedTest ? Colors.blue : Colors.yellow[200],
-                    duration: Duration(milliseconds: 300),
-                    width: 400,
-                    height: 400,
-                    alignment: Alignment.center,
-                    padding: EdgeInsets.all(0),
-                    // this isn't a great idea. allows splash, but doing it this way makes things harder to work with
-                    // e.g. you need the sized box below because alignment: up above would otherwise shrink this to the
-                    // size of the text box...
-                    child: Material(
-                      type: MaterialType.transparency,
-                      child: InkWell(
-                          onTap: () {
-                            // this is lazy and bad.
-                            setState(() {
-                              if (selectedExercise[index]) {
-                                selectedExercise[index] = false;
-                              } else {
-                                selectedExercise
-                                    .setAll(0, [false, false, false, false]);
-                                selectedExercise[index] = true;
-                              }
-                            });
-                          },
-                          splashColor: !selectedExercise[index]
-                              ? Color(0xFF1976D2)
-                              : Color(0xFF06ac51),
-                          child: SizedBox(
-                            width: 400,
-                            height: 400,
-                            child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                mainAxisSize: MainAxisSize.max,
-                                children: [
-                                  Text(exercises[index].toString(),
-                                      style: TextStyle(fontSize: 50)),
-                                ]),
-                          )),
-                    ),
-                  );
-                }),
-              ),
+            //TODO: this breaks the listview, even if it conveniently centers everything in just one line.
+            // so need to put columns withhin, not at the top :(
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 28,
+                  child:
+                      Text("Select Exercise", style: TextStyle(fontSize: 24)),
+                ),
+                SizedBox(
+                  height: 400,
+                  child: GridView.count(
+                    mainAxisSpacing: 2,
+                    crossAxisSpacing: 2,
+                    crossAxisCount: 2,
+                    children: List.generate(4, (index) {
+                      return AnimatedContainer(
+                        decoration: BoxDecoration(
+                          gradient: selectedExercise[index]
+                              ? LinearGradient(
+                                  colors: <Color>[
+                                    Color(0xFF0D47A1),
+                                    Color(0xFF1976D2),
+                                    Color(0xFF0D47A1),
+                                    //Color(0xFF42A5F5),
+                                  ],
+                                )
+                              : LinearGradient(
+                                  colors: <Color>[
+                                    Color(0xFF058f43),
+                                    Color(0xFF06ac51),
+                                    Color(0xFF058f43),
+                                    //Color(0xFF1bcc50),
+                                  ],
+                                ),
+                        ),
+                        //color: isPickedTest ? Colors.blue : Colors.yellow[200],
+                        duration: Duration(milliseconds: 300),
+                        width: 400,
+                        height: 400,
+                        alignment: Alignment.center,
+                        padding: EdgeInsets.all(0),
+                        // this isn't a great idea. allows splash, but doing it this way makes things harder to work with
+                        // e.g. you need the sized box below because alignment: up above would otherwise shrink this to the
+                        // size of the text box...
+                        child: Material(
+                          type: MaterialType.transparency,
+                          child: InkWell(
+                              onTap: () {
+                                // this is lazy and bad.
+                                setState(() {
+                                  if (selectedExercise[index]) {
+                                    selectedExercise[index] = false;
+                                  } else {
+                                    selectedExercise.setAll(
+                                        0, [false, false, false, false]);
+                                    selectedExercise[index] = true;
+                                  }
+                                });
+                              },
+                              splashColor: !selectedExercise[index]
+                                  ? Color(0xFF1976D2)
+                                  : Color(0xFF06ac51),
+                              child: SizedBox(
+                                width: 400,
+                                height: 400,
+                                child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    mainAxisSize: MainAxisSize.max,
+                                    children: [
+                                      Text(exercises[index].toString(),
+                                          style: TextStyle(fontSize: 50)),
+                                    ]),
+                              )),
+                        ),
+                      );
+                    }),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                SizedBox(
+                  height: 80,
+                  width: 380,
+                  child: TextFormField(
+                      decoration: new InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color(0xFF06ac51),
+                            width: 1.0,
+                            style: BorderStyle.solid,
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Color(0xFF1976D2), width: 1.0),
+                        ),
+                        labelText: "Select Program",
+                      ),
+                      readOnly: true,
+                      style: TextStyle(fontSize: 50),
+                      onTap: () {}),
+                ),
+                SizedBox(height: 10),
+                RaisedButton(
+                  child: Text("Go!", style: TextStyle(fontSize: 50)),
+                  onPressed: !readyToGo ? null : () => print("ready to go"),
+                  disabledColor: Color(0xFF06ac51),
+                  color: Color(0xFF1976D2),
+                )
+              ],
             ),
           ],
         ));
