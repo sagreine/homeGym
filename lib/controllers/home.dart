f47f96d172958a13e3043f98aaa80ef97d3082e0
Scott <sagreine@gmail.com>
2020-08-24 20:44:55 -0400

    untested, cast either way is in

--- lib/controllers/home.dart
+++ lib/controllers/home.dart
@@ -22,10 +22,10 @@ class HomeController {
 
   ExerciseDayController exerciseDayController = new ExerciseDayController();
 
-  Future<String> getVideo(bool isLocalTesting, BuildContext context) async {
+  Future<String> getVideo(bool recordNewVideo, BuildContext context) async {
     var exercise = Provider.of<ExerciseSet>(context, listen: false);
     var url;
-    if (!isLocalTesting) {
+    if (recordNewVideo) {
       final picker = ImagePicker();
       // TODO: doesn't handle if they press back
       final pickedFile = await picker.getVideo(source: ImageSource.camera);
@@ -69,7 +69,10 @@ class HomeController {
 
   // or just don't wait? once we send the video there's nothing
   // stoppping us from retrieving and updating the app right?
-  castMediaTo(RemoteMediaPlayer player, BuildContext context) async {
+  castMediaTo(
+      {RemoteMediaPlayer player,
+      BuildContext context,
+      @required bool recordNewVideo}) async {
     var exercise = Provider.of<ExerciseSet>(context, listen: false);
     var thisDay = Provider.of<ExerciseDay>(context, listen: false);
     String thisExercise = json.encode(exercise.toJson());
@@ -77,7 +80,8 @@ class HomeController {
     String nextExercise = json.encode(exercise.toJson());
     String thisDayJSON = json.encode(thisDay.toJson());
 
-    //String url = await getVideo(false, context);
+    // TODO: do we want to delay at all if not recording?
+    // that is, give them time to do the actual exercise?
 
     await FlutterFling.play(
       (state, condition, position) {
@@ -89,7 +93,7 @@ class HomeController {
         }
       },
       player: player,
-      mediaUri: await getVideo(false, context), // url,
+      mediaUri: await getVideo(recordNewVideo, context), // url,
       mediaTitle: thisExercise +
           nextExercise +
           thisDayJSON, //json.encode(exercise.toJson()),
