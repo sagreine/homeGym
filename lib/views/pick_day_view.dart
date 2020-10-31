5897bb43cde88ef84675909a7d842b3470e1bd82
Scott <sagreine@gmail.com>
2020-10-31 18:31:01 -0400

    removing color frokm pick day

--- lib/views/pick_day_view.dart
+++ lib/views/pick_day_view.dart
@@ -32,15 +32,64 @@ class _PickDayViewState extends State<PickDayView> {
           Expanded(
             flex: 5,
             child: GridView.count(
+              //padding: EdgeInsets.all(5),
               shrinkWrap: true,
               mainAxisSpacing: 2,
               crossAxisSpacing: 2,
               crossAxisCount: 2,
               childAspectRatio: (itemWidth / itemHeight),
               children: List.generate(4, (index) {
-                return AnimatedContainer(
-                  decoration: BoxDecoration(
-                    gradient: !pickDayController.selectedExercise[index]
+                return
+
+                    //return
+                    Material(
+                        elevation:
+                            !pickDayController.selectedExercise[index] ? 60 : 0,
+                        child: AnimatedContainer(
+                          decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color:
+                                    !pickDayController.selectedExercise[index]
+                                        ? Theme.of(context)
+                                            .canvasColor //Colors.transparent
+                                        : Theme.of(context).backgroundColor,
+                              )
+                            ],
+                            color: !pickDayController.selectedExercise[index]
+                                ? Theme.of(context).canvasColor
+                                : Theme.of(context).backgroundColor,
+                            border: Border(
+                              left: BorderSide(color: Colors.blueGrey
+                                  /*color: !pickDayController
+                                          .selectedExercise[index]
+                                      ? Colors.blueGrey
+                                      : Colors.greenAccent*/ //Color(0xFF06ac51)
+                                  ),
+                              right: BorderSide(color: Colors.blueGrey
+                                  /*color: !pickDayController
+                                          .selectedExercise[index]
+                                      ? Colors.blueGrey
+                                      : Colors.greenAccent*/ //Color(0xFF06ac51)
+                                  ),
+                              top: BorderSide(color: Colors.blueGrey
+                                  /*color: !pickDayController
+                                          .selectedExercise[index]
+                                      ? Colors.blueGrey
+                                      : Colors.greenAccent*/ //Color(0xFF06ac51)
+                                  ),
+                              bottom: BorderSide(color: Colors.blueGrey
+                                  /*
+                                  color: !pickDayController
+                                          .selectedExercise[index]
+                                      ? Colors.blueGrey
+                                      : Colors.greenAccent */
+                                  //Color(0xFF06ac51)
+                                  ),
+                            ),
+                            /*gradient: 
+                            // TODO: maybe do a grey-gunmetal gradient for when selected looks cool maybe? but dosn't match the Day view
+                            // but it already doesn't for some reason soooo.
                         ? LinearGradient(
                             colors: <Color>[
                               Color(0xFF0D47A1),
@@ -56,46 +105,52 @@ class _PickDayViewState extends State<PickDayView> {
                               Color(0xFF058f43),
                               //Color(0xFF1bcc50),
                             ],
+                          ),*/
                           ),
-                  ),
-                  //color: isPickedTest ? Colors.blue : Colors.yellow[200],
-                  duration: Duration(milliseconds: 300),
-                  alignment: Alignment.center,
-                  padding: EdgeInsets.all(0),
+                          //color: isPickedTest ? Colors.blue : Colors.yellow[200],
+                          duration: Duration(milliseconds: 300),
+                          alignment: Alignment.center,
+                          padding: EdgeInsets.all(0),
 
-                  child: Material(
-                    type: MaterialType.transparency,
-                    child: InkWell(
-                      onTap: () {
-                        // this is lazy and bad.
-                        setState(() {
-                          pickDayController.pickExercise(index);
-                        });
-                      },
-                      splashColor: !pickDayController.selectedExercise[index]
-                          ? Color(0xFF06ac51)
-                          : Color(0xFF1976D2),
-                      child:
-                          // this allows splash and clickable on the whole area of the box (max x max),
-                          // because alignment: up above would otherwise shrink this to the size of the text box...
-                          Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              mainAxisSize: MainAxisSize.max,
-                              children: [
-                            Row(
-                                mainAxisSize: MainAxisSize.max,
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Text(
-                                      pickDayController.exercises[index]
-                                          .toString(),
-                                      style: TextStyle(fontSize: 50)),
-                                ]),
-                          ]),
-                    ),
-                  ),
-                );
+                          child: Material(
+                            type: MaterialType.transparency,
+                            child: InkWell(
+                              onTap: () {
+                                // this is lazy and bad.
+                                setState(() {
+                                  pickDayController.pickExercise(index);
+                                });
+                              },
+                              splashColor:
+                                  !pickDayController.selectedExercise[index]
+                                      ? Theme.of(context).backgroundColor
+                                      : Theme.of(context).canvasColor,
+                              /* ? Color(0xFF06ac51)
+                                      : Color(0xFF1976D2),*/
+                              child:
+                                  // this allows splash and clickable on the whole area of the box (max x max),
+                                  // because alignment: up above would otherwise shrink this to the size of the text box...
+                                  Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
+                                      mainAxisSize: MainAxisSize.max,
+                                      children: [
+                                    Row(
+                                        mainAxisSize: MainAxisSize.max,
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: [
+                                          Text(
+                                              pickDayController.exercises[index]
+                                                  .toString(),
+                                              style: TextStyle(fontSize: 50)),
+                                        ]),
+                                  ]),
+                            ),
+                          ),
+                        ));
               }),
             ),
           ),
@@ -108,13 +163,16 @@ class _PickDayViewState extends State<PickDayView> {
             decoration: new InputDecoration(
               enabledBorder: OutlineInputBorder(
                 borderSide: BorderSide(
-                  color: Color(0xFF1976D2),
+                  color: Colors.blueGrey, //Color(0xFF1976D2),
                   width: 1.0,
                   style: BorderStyle.solid,
                 ),
               ),
               focusedBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: Color(0xFF06ac51), width: 1.0),
+                borderSide: BorderSide(
+                    color: Colors.blueGrey,
+                    //Color(0xFF06ac51),
+                    width: 1.0),
               ),
               labelText: "Select Program",
             ),
@@ -136,8 +194,8 @@ class _PickDayViewState extends State<PickDayView> {
             onPressed: !pickDayController.readyToGo
                 ? null
                 : () => pickDayController.launchDay(context),
-            disabledColor: Color(0xFF1976D2),
-            color: Color(0xFF06ac51),
+            disabledColor: Theme.of(context).canvasColor,
+            color: Theme.of(context).backgroundColor,
             //)
           ),
           SizedBox(height: 10),
